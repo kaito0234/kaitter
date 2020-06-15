f6daf925f282c2357c4f9a61adebcae5b48f3ab6
kaito0234 <uminchu1990@gmail.com>
2020-06-15 12:01:07 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,6 @@ User.create!(name: "k.k",
               admin: true)
 
 99.times do |n|
-
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -29,3 +28,12 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+#リレーションシップ
+
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
