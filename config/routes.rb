8df89c2de84eb83ed38afbc031a14ed5a725c186
kaito0234 <uminchu1990@gmail.com>
2020-06-24 12:35:22 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
+  get  '/icon',   to: 'static_pages#icon'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
