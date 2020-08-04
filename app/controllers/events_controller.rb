9cbc87cedbdcbe9420fde82367f0a917574440a1
kaito0234 <uminchu1990@gmail.com>
2020-08-04 14:23:56 +0900

    2020.0804.2323

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,7 +64,7 @@ class EventsController < ApplicationController
     # end
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: '予定を更新しました!' }
+        format.html { redirect_to user_event_path(current_user,@event), notice: '予定を更新しました!' }
         format.json { render :index, status: :ok, location: @event }
       else
         format.html { render :edit }
