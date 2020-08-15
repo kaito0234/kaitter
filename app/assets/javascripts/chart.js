eace1df8177b1a1ae3e7b0bd97d70e6f65aba9bc
kaito0234 <uminchu1990@gmail.com>
2020-08-15 13:00:31 +0900

    condition_8.16

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -3,67 +3,67 @@ const condutionData = {
   date: gon.timedata,
 };
 
-const loadCharts = function () {
-  const chartDataSet = {
-    type: 'line',
-    data: {
-      datasets: [{
-        label: 'line',
-        data: [],
-        type: 'line'
-      }, {
-        label: 'bar',
-        data: [],
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      maintainAspectRatio: false,
-      tooltips: {
-        enabled: true,
-        mode: 'single',
-        callbacks: {
-          afterLabel: function(tooltipItem, data) { 
-            return gon.memo[tooltipItem.index];
-          }
-        }
-      },
-      scales: {
-        xAxes: [{
-          type: 'time',
-          distribution: 'linear',
-          ticks: {
-            source: 'data'
-          }
-          // time: {
-          //   unit: 'hour',
-          //   fomat: 'H'
-          // }
-        }],
-        yAxes: [{
-          ticks: {
-            beginAtZero: true,
-            max: 100
-          }
-        }]
-      }
-    }
-  };
-  for (let i = 0; i < condutionData.level.length; i++) {
-    chartDataSet.data.datasets[0].data.push({
-      y: condutionData.level[i],
-      x: new Date(condutionData.date[i])
-    });
-  }
+// const loadCharts = function () {
+//   const chartDataSet = {
+//     type: 'line',
+//     data: {
+//       datasets: [{
+//         label: 'line',
+//         data: [],
+//         type: 'line'
+//       }, {
+//         label: 'bar',
+//         data: [],
+//         backgroundColor: 'rgba(75, 192, 192, 0.2)',
+//         borderColor: 'rgba(75, 192, 192, 1)',
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       maintainAspectRatio: false,
+//       tooltips: {
+//         enabled: true,
+//         mode: 'single',
+//         callbacks: {
+//           afterLabel: function(tooltipItem, data) { 
+//             return gon.memo[tooltipItem.index];
+//           }
+//         }
+//       },
+//       scales: {
+//         xAxes: [{
+//           type: 'time',
+//           distribution: 'linear',
+//           ticks: {
+//             source: 'data'
+//           }
+//           // time: {
+//           //   unit: 'hour',
+//           //   fomat: 'H'
+//           // }
+//         }],
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero: true,
+//             max: 100
+//           }
+//         }]
+//       }
+//     }
+//   };
+//   for (let i = 0; i < condutionData.level.length; i++) {
+//     chartDataSet.data.datasets[0].data.push({
+//       y: condutionData.level[i],
+//       x: new Date(condutionData.date[i])
+//     });
+//   }
   
-  const ctx = document.createElement('canvas');
-  document.getElementById('chart-area').appendChild(ctx);
-  new Chart(ctx, chartDataSet);
-};
+//   const ctx = document.createElement('canvas');
+//   document.getElementById('chart-area').appendChild(ctx);
+//   new Chart(ctx, chartDataSet);
+// };
 
-loadCharts();
+// loadCharts();
 
 
 
