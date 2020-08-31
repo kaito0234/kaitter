7ad0ec355d0d9790512538a796872e9e69298cb7
kaito0234 <uminchu1990@gmail.com>
2020-08-31 13:10:37 +0900

    condition_datetime_8.31

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -210,7 +210,7 @@ class ConditionsController < ApplicationController
       data = timedata[0]
       gon.timedata << data
     end
-    render 'index_month'
+    render 'index_week'
   end
   def month_avg_admin
     @date = Time.current.beginning_of_month
